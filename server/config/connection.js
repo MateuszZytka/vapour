644b5e01c8c432e434dc1130290f4ad4d1aa8374
MateuszZytka <109117198+MateuszZytka@users.noreply.github.com>
2023-01-11 15:53:42 -0500

    strictquery was deprecated

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 
+mongoose.set("strictQuery", false);
+
 mongoose.connect(
     process.env.MONGODB_URI || 'mongodb+srv://vapour:vapour123@cluster0.k7nw0g1.mongodb.net/vapour',
     {
