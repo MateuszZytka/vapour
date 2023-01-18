10b46bca4c127d593b1c26944e0bf16ac6f44f87
MateuszZytka <109117198+MateuszZytka@users.noreply.github.com>
2023-01-18 14:22:10 -0500

    changed mongodb server

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 mongoose.set("strictQuery", false);
 
 mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb+srv://vapour:vapour123@cluster0.k7nw0g1.mongodb.net/vapour',
+    process.env.MONGODB_URI || 'mongodb+srv://vapour:vapour123@cluster0.k7nw0g1.mongodb.net/?retryWrites=true&w=majority',
     {
         useNewUrlParser: true,
         useUnifiedTopology: true
